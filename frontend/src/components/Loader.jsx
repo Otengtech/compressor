f9cae234478f4fe52c16f03b2be506aa9119f173
Otengtech <otengebenezer326@gmail.com>
2025-12-10 22:20:05 -0800

    COMMIT

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="w-full h-screen bg-green-100 flex flex-col items-center justify-center">
+    <div className="w-full h-[90vh] bg-green-100 flex flex-col items-center justify-center">
       <div className="animate-spin rounded-full h-14 w-14 border-4 border-green-600 border-t-transparent"></div>
       <div className="mt-6">
         <span className="text-green-600 text-sm font-medium tracking-wider">
